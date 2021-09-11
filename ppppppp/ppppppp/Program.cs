15bce2a711734c3ecc4dd4c767558f17bd2d6a82
443-19 <443-19@C41402>
2021-09-11 11:54:58 +0300

    a

---
+++ ppppppp/ppppppp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppppppp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter word ");
+            var str = Console.ReadLine();
+            for (int i = 0; i < str.Length; ++i)
+                {
+                int left = i - 1; int right = i + 1;
+                
+            }
+        }
+    }
+}
