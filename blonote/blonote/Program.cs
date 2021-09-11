d5aa888236579b87846ffd24cceb0afce9f5daa5
443-19 <443-19@C41402>
2021-09-11 15:54:40 +0300

    кo

--- blonote/blonote/Program.cs
+++ blonote/blonote/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace blonote
 {
@@ -10,6 +11,10 @@ namespace blonote
     {
         static void Main(string[] args)
         {
+            FileInfo fi = new FileInfo(@"X:\one.txt");
+            if (fi != null)
+                Console.WriteLine(String.Format("Information about file: {0}, {1} bytes, last modified on {2} - Full path: {3}", fi.Name, fi.Length, fi.LastWriteTime, fi.FullName));
+            Console.ReadKey();
         }
     }
 }
